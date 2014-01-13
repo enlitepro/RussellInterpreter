a5c476e50efc7a6791c8e6308c149638f30059b0
dzyanis <belinfonetkuzyakin>
2014-01-13 17:25:57 +0300

    Empty test for Lexer

--- tests/RussellInterpreterTest/LexerTest.php
+++ tests/RussellInterpreterTest/LexerTest.php
@@ -2,7 +2,7 @@
 
 //use RussellInterpreter;
 
-class ParserTest
+class LexerTest
     extends PHPUnit_Framework_TestCase
 {
     /**
@@ -12,55 +12,55 @@ class ParserTest
 
     public function setUp()
     {
-        $this->object = new RussellInterpreter\Parser();
+        $this->object = new RussellInterpreter\Lexer();
     }
 
     public function testSetGetScalarText()
     {
-        $text = 'hello';
-        $key = $this->object->setScalarText($text);
-        $this->assertEquals($text, $this->object->getScalarText($key));
+//        $text = 'hello';
+//        $key = $this->object->setScalarText($text);
+//        $this->assertEquals($text, $this->object->getScalarText($key));
     }
 
     public function testParserString()
     {
-        $this->object->reset();
-
-        $code = <<<EOL
-concatenation('This is text', '!', '')
-    concatenation('"', '
-', '"') variable(str, 'string')
-EOL;
-
-        $code = $this->object->parseScalarText($code);
-        $this->assertAttributeCount(6, 'tokens', $this->object);
-        $this->assertNotEmpty($code);
+//        $this->object->reset();
+//
+//        $code = <<<EOL
+//concatenation('This is text', '!', '')
+//    concatenation('"', '
+//', '"') variable(str, 'string')
+//EOL;
+//
+//        $code = $this->object->parseScalarText($code);
+//        $this->assertAttributeCount(6, 'tokens', $this->object);
+//        $this->assertNotEmpty($code);
     }
 
     public function testParseFunctions()
     {
-        $code = <<<EOL
-functionA(
-  functionAA(1, 2, 3),
-  functionAB(
-    functionABA(4, 5),
-    functionABB(6)
-  )
-)
-functionB()
-functionC(7,89)
-functionD(
-    functionDA(
-        functionDAA(
-            functionDAAA()
-        )
-    )
-)
-EOL;
-
-        $code = $this->object->removeUnnecessarySymbols($code);
-        $code = $this->object->parseFunctions($code);
-        $this->assertNotEmpty($code);
+//        $code = <<<EOL
+//functionA(
+//  functionAA(1, 2, 3),
+//  functionAB(
+//    functionABA(4, 5),
+//    functionABB(6)
+//  )
+//)
+//functionB()
+//functionC(7,89)
+//functionD(
+//    functionDA(
+//        functionDAA(
+//            functionDAAA()
+//        )
+//    )
+//)
+//EOL;
+//
+//        $code = $this->object->removeUnnecessarySymbols($code);
+//        $code = $this->object->parseFunctions($code);
+//        $this->assertNotEmpty($code);
     }
 
     /**
@@ -68,10 +68,10 @@ EOL;
      */
     public function testParseArguments($text, $arguments)
     {
-        $result = $this->object->parseArguments($text);
-        foreach ($arguments as $key => $argument) {
-            $this->assertEquals($argument, $result[$key]);
-        }
+//        $result = $this->object->parseArguments($text);
+//        foreach ($arguments as $key => $argument) {
+//            $this->assertEquals($argument, $result[$key]);
+//        }
     }
 
     public function providerParserArguments()
@@ -104,9 +104,9 @@ EOL;
      */
     public function testParseFunction($function, $name, $args)
     {
-        $struct = $this->object->parseFunction($function);
-        $this->assertEquals($struct['name'], $name);
-        $this->assertEquals($struct['arguments'], $args);
+//        $struct = $this->object->parseFunction($function);
+//        $this->assertEquals($struct['name'], $name);
+//        $this->assertEquals($struct['arguments'], $args);
     }
 
     public function providerParseFunction()
